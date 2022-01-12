c4f2a9b2c64b07d7d9b7496d6960e95936980ea6
Vladimir Prilepskiy <a89122285824@gmail.com>
2022-01-12 11:55:38 +0500

    Add files via upload

---
+++ src/main/java/ru/prilepsky/MainClass.java
@@ -0,0 +1,17 @@
+package ru.prilepsky;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+/**
+ * @author Vladimir Prilepskiy
+ * @created 12.01.2022
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        JacksonJsonProvider jacksonJsonProvider = new JacksonJsonProvider();
+
+    }
+
+}
